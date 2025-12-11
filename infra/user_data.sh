086b780f18cf638b0dbefeaeaf439485af18231c
AndyAlarconldu <andyjavier301067@gmail.com>
2025-12-11 18:35:12 -0500

    ADD USER DATA

--- infra/user_data.sh
+++ infra/user_data.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 yum update -y
 yum install -y docker
-systemctl start docker
 systemctl enable docker
+systemctl start docker
 
-docker run -d -p 80:80 --name app ${DOCKER_IMAGE}
+docker stop app || true
+docker rm app || true
+
+docker run -d --name app -p 80:80 ${DOCKER_IMAGE}
